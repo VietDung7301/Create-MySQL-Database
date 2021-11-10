2e56f026bb03338fda10eff9e9091336325e227a
VietDung7301 <vietdung.7301@gmail.com>
2021-11-10 16:02:45 +0700

    Thêm file JDBC và sửa lỗi

--- initDB.java
+++ initDB.java
@@ -2,31 +2,36 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
 
-public class jdbc {
+public class initDB {
     public static void main(String[] args) 
     {
         Connection conn = null;
-        String url = "jdbc:mysql://localhost:3306/Lab01";
+        String url = "jdbc:mysql://localhost:3306/DatabaseLab";
         String user = "root";
         String password = "#nothingspecial";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url 
-                    + "?useUnicode=true&characterEncoding=utf-8", user, password);
+            conn = DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String sql = "show tables";
+        String sql = "CREATE TABLE KHACH_HANG ("+
+                    "ID_KH VARCHAR (30) NOT NULL," +
+                    "SDT_KH VARCHAR (20) NOT NULL," +
+                    "TEN_KH VARCHAR (50) NOT NULL," +
+                    "DIA_CHI_KH VARCHAR (100) NOT NULL," +
+                    "GMAIL_KH VARCHAR (50) NOT NULL," +
+                    "CONSTRAINT PK_KH PRIMARY KEY (ID_KH))";
         try {
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString("Tables_in_Lab01"));
-            }
-        } catch (Exception e) {
+            Statement st = conn.createStatement();
+            st.executeUpdate(sql);
+            System.out.println("**** Created table KHACH_HANG ****");
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
+         } 
     }
 }
